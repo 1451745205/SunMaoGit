ac1353f260c1076496bc0ff60f2da82da7355f39
1451745205 <1451745205@qq.com>
2024-01-13 17:23:02 +0800

    init unity project

---
+++ Assets/Scenes/LeanUGUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Mytools/CreateAssetMenu")]
+public class LeanUGUI : ScriptableObject
+{
+    public Transform PeopleObj;
+}
