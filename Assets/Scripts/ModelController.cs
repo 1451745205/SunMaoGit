ddfcb6548ae8086b79ce3bf41804a07277077ad5
1451745205 <1451745205@qq.com>
2024-03-18 23:50:44 +0800

    模型单击高亮，双击消失已完成03-18

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class ModelController : MonoBehaviour
 {
-	public GameObject[] smallmodels;
+	public GameObject[] smallmodels;  //可以被点击的小模型
+
+	public Material secondMaterial; // 第二个材质球
+
+	Renderer currentRenderer; // 当前选定的Renderer
+	Material[] originalMaterials; // 当前选定的Renderer原始的材质球数组
+
 
 	private bool onDrag = false;  //是否被拖拽    
 	public float speed = 6f;      //旋转速度    
@@ -66,8 +72,8 @@ public class ModelController : MonoBehaviour
 			tempSpeed = 0f;
 		}
 
-		//执行隐藏小方块模型的方法
-		CloseModel();
+		//执行模型高亮及隐藏小方块模型的方法
+		HightlightAndCloseModel();
 	}
 
 	void OnMouseUp()
@@ -76,9 +82,9 @@ public class ModelController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// 隐藏小方块模型
+	/// 模型高亮 及 隐藏小方块模型
 	/// </summary>
-	public void CloseModel()
+	public void HightlightAndCloseModel()
 	{
 		// 使用射线检测与鼠标交互
 		if (Input.GetMouseButtonDown(0))
@@ -86,23 +92,81 @@ public class ModelController : MonoBehaviour
 			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+			LayerMask layerMask = LayerMask.GetMask("SmallModel");
+
 			// 如果射线击中了物体
-			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
 			{
-				// 遍历数组查找被点击的模型
-				foreach (GameObject model in smallmodels)
+				Debug.Log(hit.transform.gameObject.layer);
+				
+
+				Renderer hitRenderer = hit.transform.gameObject.GetComponent<Renderer>();
+
+				if (hitRenderer != currentRenderer)
 				{
-					Debug.Log(hit.transform.gameObject.name);
-					if (model == hit.transform.gameObject)
+					// 移除上一个选定模型的第二个材质球
+					if (currentRenderer != null && originalMaterials != null)
 					{
-						// 点击到模型时，销毁该模型
-						model.SetActive(false);
-						break; // 找到并销毁后跳出循环
+						currentRenderer.materials = originalMaterials;
 					}
+
+					// 记录当前选定的模型和其原始材质球
+					currentRenderer = hitRenderer;
+					originalMaterials = hitRenderer.materials;
+
+					// 添加第二个材质球到当前选定的模型
+					Material[] newMaterials = new Material[originalMaterials.Length + 1];
+					originalMaterials.CopyTo(newMaterials, 0);
+					newMaterials[originalMaterials.Length] = secondMaterial;
+
+					currentRenderer.materials = newMaterials;
 				}
+				else
+				{
+					// 如果再次点击同一模型，则隐藏该模型
+					hit.transform.gameObject.SetActive(false);
+					currentRenderer = null;
+					originalMaterials = null;
+				}
+
+				//// 遍历数组查找被点击的模型
+				//foreach (GameObject model in smallmodels)
+				//{
+				//	if (model == hit.transform.gameObject)
+				//	{
+				//		Renderer hitRenderer = hit.transform.gameObject.GetComponent<Renderer>();
+
+				//		if (hitRenderer != currentRenderer)
+				//		{
+				//			// 移除上一个选定模型的第二个材质球
+				//			if (currentRenderer != null && originalMaterials != null)
+				//			{
+				//				currentRenderer.materials = originalMaterials;
+				//			}
+
+				//			// 记录当前选定的模型和其原始材质球
+				//			currentRenderer = hitRenderer;
+				//			originalMaterials = hitRenderer.materials;
+
+				//			// 添加第二个材质球到当前选定的模型
+				//			Material[] newMaterials = new Material[originalMaterials.Length + 1];
+				//			originalMaterials.CopyTo(newMaterials, 0);
+				//			newMaterials[originalMaterials.Length] = secondMaterial;
+
+				//			currentRenderer.materials = newMaterials;
+				//		}
+				//		else
+				//		{
+				//			// 如果再次点击同一模型，则隐藏该模型
+				//			hit.transform.gameObject.SetActive(false);
+				//			currentRenderer = null;
+				//			originalMaterials = null;
+				//		}
+
+				//		break; // 找到并处理后跳出循环
+				//	}
+				//}
 			}
 		}
 	}
-
-
 }
